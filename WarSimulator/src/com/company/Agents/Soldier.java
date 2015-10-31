ee477fa62d331a76498088ea4e1cfdef82e1efb9
merdigon <szymon.nowak3848@gmail.com>
2015-10-31 17:27:20 +0100

    Dodany jade

--- WarSimulator/src/com/company/Agents/Soldier.java
+++ WarSimulator/src/com/company/Agents/Soldier.java
@@ -3,9 +3,16 @@ package com.company.Agents;
 /**
  * Created by Szymon on 2015-10-16.
  */
+import jade.core.Agent;
+
 public class Soldier extends Agent {
     Command comm;
 
+    @Override
+    protected void setup()
+    {
+        addBehaviour(new SoldierBehaviour());
+    }
 
     public void setCommand(Command comm)
     {
---
+++ WarSimulator/src/com/company/Agents/SoldierBehaviour.java
@@ -0,0 +1,14 @@
+package com.company.Agents;
+
+/**
+ * Created by Szymon on 2015-10-31.
+ */
+import jade.core.behaviours.CyclicBehaviour;
+
+public class SoldierBehaviour extends CyclicBehaviour{
+
+    @Override
+    public void action() {
+
+    }
+}
