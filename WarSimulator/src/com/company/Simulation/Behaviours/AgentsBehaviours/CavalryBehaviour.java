01839f8733c7f7059c0389fd2a6f19eb63005b83
merdigon <szymon.nowak3848@gmail.com>
2015-12-15 00:29:19 +0100

    Kolejne

--- WarSimulator/src/com/company/Simulation/Behaviours/AgentsBehaviours/CavalryBehaviour.java
+++ WarSimulator/src/com/company/Simulation/Behaviours/AgentsBehaviours/CavalryBehaviour.java
@@ -31,7 +31,7 @@ public class CavalryBehaviour extends SoldierBehaviour {
     @Override
     public void action(){
         if(!checkIfAlive()){
-            killSoldier();
+            getSoldier().killSoldier();
             return;
         }
         thinking();
